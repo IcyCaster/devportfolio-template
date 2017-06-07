7facd113ecab6cd67b8d45cbd200d46a3558e8cc
IcyCaster <chester.booker.nz@gmail.com>
2017-06-07 12:55:35 +1200

    Wider header + header show/hide on scroll functionality

--- js/scripts.js
+++ js/scripts.js
@@ -96,3 +96,37 @@
     });
 
 })(jQuery);
+
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+var didScroll;
+
+$(window).scroll(function(event) {
+    didScroll = true;
+});
+
+setInterval(function() {
+    if(didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+},250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+if (Math.abs(lastScrollTop - st) <= delta)
+  return;
+    // If current position > last position AND scrolled past navbar...
+if (st > lastScrollTop && st > navbarHeight){
+  // Scroll Down
+  $('header').removeClass('nav-down').addClass('nav-up');
+} else {
+  // Scroll Up
+  // If did not scroll past the document (possible on mac)...
+  if(st + $(window).height() < $(document).height()) { 
+    $('header').removeClass('nav-up').addClass('nav-down');
+  }
+}
+lastScrollTop = st;
+}
